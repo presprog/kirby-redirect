e89dab1989ee869980c3b8dd6346d3ba5e38cadf
Benedict Zinke <bz@presentprogressive.de>
2016-10-22 12:09:49 +0200

    remove all logic to model and remove controller and template

--- models/redirect.php
+++ models/redirect.php
@@ -2,8 +2,15 @@
 
 class RedirectPage extends Page {
   public function url() {
-    if ($this->external()->isNotEmpty()) {
+    if ($this->redirect()->isNotEmpty() && ($p = page($this->redirect()->value()))) {
+      // if 'redirect to page' is set (and exists), go for it
+      return $p->url();
+    } else if ($this->external()->isNotEmpty()) {
+      // if 'external url' is set, go for it
       return $this->external();
+    } elseif ($this->children()->visible()->count()) {
+      // else if page has visible children, redirect to the first one
+      return $this->children()->visible()->first()->url();
     }
     return parent::url();
   }
